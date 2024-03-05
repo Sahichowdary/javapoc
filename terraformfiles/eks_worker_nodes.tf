c46a4098acae30b75241adaf70fb25467d1d24eb
SAHICHOWDARY <78893457+Sahichowdary@users.noreply.github.com>
2024-03-05 12:50:27 +0530

    Update eks_worker_nodes.tf

--- terraformfiles/eks_worker_nodes.tf
+++ terraformfiles/eks_worker_nodes.tf
@@ -1,6 +1,6 @@
 resource "aws_eks_node_group" "eks_main" {
   cluster_name = aws_eks_cluster.eks_main.name
-  node_role_arn = aws_iam_role.NodeGroupRole.arn
+  node_role_arn = aws_iam_role.NodeGroupRole-poc.arn
   subnet_ids = [aws_subnet.vpc_private_subnet_private_1.id, aws_subnet.vpc_private_subnet_private_2.id]
   scaling_config {
     min_size = 3
@@ -12,8 +12,8 @@ resource "aws_eks_node_group" "eks_main" {
   #release_version = "xxx"
 
   depends_on = [
-    aws_iam_role_policy_attachment.AmazonEKSWorkerNodePolicy,
-    aws_iam_role_policy_attachment.AmazonEC2ContainerRegistryReadOnly,
-    aws_iam_role_policy_attachment.AmazonEKS_CNI_Policy
+    aws_iam_role_policy_attachment.AmazonEKSWorkerNodePolicy-poc,
+    aws_iam_role_policy_attachment.AmazonEC2ContainerRegistryReadOnly-poc,
+    aws_iam_role_policy_attachment.AmazonEKS_CNI_Policy-poc
   ]
 }
