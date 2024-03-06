fca7264555b74478a9039a73ea1eea21138d326d
SAHICHOWDARY <78893457+Sahichowdary@users.noreply.github.com>
2024-03-06 19:09:17 +0530

    Update rds.tf

--- terraformfiles/rds.tf
+++ terraformfiles/rds.tf
@@ -20,3 +20,8 @@ resource "aws_db_instance" "my-sql" {
   vpc_security_group_ids = [aws_security_group.eks_cluster_sg.id]
   depends_on = [aws_db_subnet_group.rds_subnetgroup]
 }
+
+provisioner "local-exec" {
+  command = "bash execute_sql_script.sh ${aws_db_instance.my-sql.endpoint} ${aws_db_instance.my-sql.username} ${aws_db_instance.my-sql.password}"
+}
+
