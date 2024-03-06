81213c20395aad789875179dbb8dfa67a63e23ed
SAHICHOWDARY <78893457+Sahichowdary@users.noreply.github.com>
2024-03-06 11:11:05 +0530

    Update eks_worker_nodes.tf

--- terraformfiles/eks_worker_nodes.tf
+++ terraformfiles/eks_worker_nodes.tf
@@ -1,7 +1,7 @@
 resource "aws_eks_node_group" "eks_main" {
   cluster_name = aws_eks_cluster.eks_main.name
   node_role_arn = var.eks_node_role
-  subnet_ids = [aws_subnet.vpc_private_subnet_private_1.id, aws_subnet.vpc_private_subnet_private_2.id]
+  subnet_ids = [aws_subnet.vpc_private_subnet_private_1.id, aws_subnet.vpc_private_subnet_private_2.id, aws_subnet.vpc_private_subnet_private_3.id, aws_subnet.vpc_private_subnet_private_4.id, aws_subnet.vpc_private_subnet_private_5.id, aws_subnet.vpc_private_subnet_private_6.id]
   scaling_config {
     min_size = 3
     max_size = 5
