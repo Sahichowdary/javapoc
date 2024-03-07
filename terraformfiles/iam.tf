0dd94e90240bd967cbbf4efc6a20410f99dea6da
SAHICHOWDARY <78893457+Sahichowdary@users.noreply.github.com>
2024-03-07 15:19:46 +0530

    Update iam.tf

--- terraformfiles/iam.tf
+++ terraformfiles/iam.tf
@@ -32,21 +32,21 @@ resource "aws_iam_role" "AmazonEKSNodeRole" {
 
 resource "aws_iam_role_policy_attachment" "AmazonEKSClusterPolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-  role       = aws_iam_role.EKSClusterRole2.name
+  role       = aws_iam_role.pocEKSClusterRole.name
 }
 
 resource "aws_iam_role_policy_attachment" "AmazonEKSWorkerNodePolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
-  role       = aws_iam_role.AmazonEKSNodeRole2.name
+  role       = aws_iam_role.pocAmazonEKSNodeRole.name
 }
 
 
 resource "aws_iam_role_policy_attachment" "AmazonEC2ContainerRegistryReadOnly" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-  role       = aws_iam_role.AmazonEKSNodeRole2.name
+  role       = aws_iam_role.pocAmazonEKSNodeRole.name
 }
 
 resource "aws_iam_role_policy_attachment" "AmazonEKS_CNI_Policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
-  role       = aws_iam_role.AmazonEKSNodeRole2.name
+  role       = aws_iam_role.pocAmazonEKSNodeRole.name
 }
