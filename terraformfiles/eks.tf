4200e7025acee6551a36723d1a049106505473b2
SAHICHOWDARY <78893457+Sahichowdary@users.noreply.github.com>
2024-03-07 14:19:50 +0530

    Update eks.tf

--- terraformfiles/eks.tf
+++ terraformfiles/eks.tf
@@ -7,7 +7,7 @@ resource "aws_eks_cluster" "eks_main" {
     endpoint_private_access = true
     subnet_ids = [aws_subnet.vpc_private_subnet_private_1.id, aws_subnet.vpc_private_subnet_private_2.id, aws_subnet.vpc_private_subnet_private_3.id, aws_subnet.vpc_private_subnet_private_4.id, aws_subnet.vpc_private_subnet_private_6.id, aws_subnet.vpc_private_subnet_private_6.id]
   }
-
+  
   version = "1.29"
 
   encryption_config {
@@ -17,4 +17,9 @@ resource "aws_eks_cluster" "eks_main" {
         key_arn = var.eks_encryption_key
     }
   }
+  depends_on = [
+    aws_iam_role_policy_attachment.eks_service_role_policy,
+    aws_iam_role_policy_attachment.eks_node_group_role_policy,
+    aws_iam_role_policy_attachment.eks_node_group_additional_policy
+  ]
 }
