18088978d88fd29bb7ae5988260a6bcf4bd2df47
SAHICHOWDARY <78893457+Sahichowdary@users.noreply.github.com>
2024-03-08 16:42:55 +0530

    Update eksnett.tf

--- terraformfiles/eksnett.tf
+++ terraformfiles/eksnett.tf
@@ -76,7 +76,6 @@ resource "aws_iam_role_policy_attachment" "eks-poc-main-AmazonEC2ContainerRegist
 #creating node group for EKS
 resource "aws_eks_node_group" "eks-node-group" {
   cluster_name    = aws_eks_cluster.eks-poc-main.name
-  name = eks-node-group
   node_role_arn   = aws_iam_role.eks-nodes.arn
   subnet_ids      = [aws_subnet.vpc_private_subnet_private_1.id]
 
