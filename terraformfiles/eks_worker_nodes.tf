c624486c1af2e951e72ba574b64d1da8e05bd616
SAHICHOWDARY <78893457+Sahichowdary@users.noreply.github.com>
2024-03-08 10:45:54 +0530

    Update eks_worker_nodes.tf

--- terraformfiles/eks_worker_nodes.tf
+++ terraformfiles/eks_worker_nodes.tf
@@ -8,10 +8,23 @@ resource "aws_eks_node_group" "eks_poc_main" {
     desired_size = 3
   }
   instance_types = ["t3a.medium"]
+  capacity_type  = "ON_DEMAND"    # or SPOT
+  disk_size       = 20             # in GB
   version = "1.29"
   #release_version = "xxx"
+  remote_access {
+    ec2_ssh_key = "aws-poc-demo"
+  }
   depends_on = [
     aws_iam_role_policy_attachment.AmazonEKSWorkerNodePolicy
   ]
+}
 
+resource "aws_eks_node_group_update_config" "node_group_update" {
+  cluster_name    = aws_eks_cluster.eks_poc_main.name
+  node_group_name = aws_eks_node_group.eks_poc_main.name
+  version         = "1.29"  # The Kubernetes version for which to enable auto-upgrades
+
+  enabled    = true
+  wait_for_completion = true
 }
