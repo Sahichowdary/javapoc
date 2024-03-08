0cb6754fdb7bd9f11b27223c24defdd02643ceff
SAHICHOWDARY <78893457+Sahichowdary@users.noreply.github.com>
2024-03-08 19:19:18 +0530

    Update eks_security_groups.tf

--- terraformfiles/eks_security_groups.tf
+++ terraformfiles/eks_security_groups.tf
@@ -18,3 +18,20 @@ resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv6" {
     security_group_id = aws_security_group.eks_cluster_sg.id
     cidr_ipv6       = "::/0"
 }
+
+
+resource "aws_vpc_security_group_ingress_rule" "allow_all_traffic_ipv4" { 
+    from_port = 0
+    to_port = 65535
+    ip_protocol = "all"
+    security_group_id = aws_security_group.eks_cluster_sg.id
+    cidr_ipv4         = "0.0.0.0/0"
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_all_traffic_ipv6" {
+    from_port = 0
+    to_port = 65535
+    ip_protocol = "all"
+    security_group_id = aws_security_group.eks_cluster_sg.id
+    cidr_ipv6       = "::/0"
+}
