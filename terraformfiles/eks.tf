05fa4286b2694d106088c0fe8ca5df086044cd4f
SAHICHOWDARY <78893457+Sahichowdary@users.noreply.github.com>
2024-03-08 10:25:16 +0530

    Update eks.tf

--- terraformfiles/eks.tf
+++ terraformfiles/eks.tf
@@ -5,7 +5,7 @@ resource "aws_eks_cluster" "eks_poc_main" {
   vpc_config {
     endpoint_public_access = false
     endpoint_private_access = true
-    subnet_ids = [aws_subnet.vpc_private_subnet_private_1.id, aws_subnet.vpc_private_subnet_private_2.id, aws_subnet.vpc_private_subnet_private_3.id, aws_subnet.vpc_private_subnet_private_4.id, aws_subnet.vpc_private_subnet_private_6.id, aws_subnet.vpc_private_subnet_private_6.id]
+    subnet_ids = [aws_subnet.vpc_private_subnet_private_1.id, aws_subnet.vpc_private_subnet_private_2.id, aws_subnet.vpc_private_subnet_private_3.id, aws_subnet.vpc_private_subnet_private_4.id]
   }
   
   version = "1.29"
