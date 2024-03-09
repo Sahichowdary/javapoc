8671d103ab46c86171f451cede20619e9dff13a6
SAHICHOWDARY <78893457+Sahichowdary@users.noreply.github.com>
2024-03-09 11:55:59 +0530

    Update vpc_private.tf

--- terraformfiles/vpc_private.tf
+++ terraformfiles/vpc_private.tf
@@ -20,8 +20,7 @@ resource "aws_subnet" "vpc_private_subnet_private_1" {
 
 resource "aws_route_table" "private_vpc_route_table" {
   vpc_id = aws_vpc.vpc_private.id
-
-     route = [
+  route = [
     {
       cidr_block                 = "0.0.0.0/0"
       nat_gateway_id             = aws_nat_gateway.nat.id
