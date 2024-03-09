24c9840c815230edda41af9dc6bcb67bdf266946
SAHICHOWDARY <78893457+Sahichowdary@users.noreply.github.com>
2024-03-09 13:51:06 +0530

    Update main.tf

--- terraformfiles/main.tf
+++ terraformfiles/main.tf
@@ -1,14 +1,13 @@
-provider "aws" {
-  region = var.region
-  alias  = "use_default_region"
-}
-
-
 terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.0"
+      version = "~> 5.0"
     }
   }
+}
+
+provider "aws" {
+  region = var.region
+  alias  = "use_default_region"
 }
