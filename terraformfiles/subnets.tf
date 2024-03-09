6456700f639ba9a3ea607667d5ceb6ad2dd3285e
SAHICHOWDARY <78893457+Sahichowdary@users.noreply.github.com>
2024-03-09 14:45:32 +0530

    Create subnets.tf

---
+++ terraformfiles/subnets.tf
@@ -0,0 +1,49 @@
+resource "aws_subnet" "private-us-east-1a" {
+  vpc_id            = aws_vpc.vpc_private.id
+  cidr_block        = "172.34.0.0/19"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    "Name"                            = "private-us-east-1a"
+    "kubernetes.io/role/internal-elb" = "1"
+    "kubernetes.io/cluster/demo"      = "owned"
+  }
+}
+
+resource "aws_subnet" "private-us-east-1b" {
+  vpc_id            = aws_vpc.vpc_private.id
+  cidr_block        = "172.34.32.0/19"
+  availability_zone = "us-east-1b"
+
+  tags = {
+    "Name"                            = "private-us-east-1b"
+    "kubernetes.io/role/internal-elb" = "1"
+    "kubernetes.io/cluster/demo"      = "owned"
+  }
+}
+
+resource "aws_subnet" "public-us-east-1a" {
+  vpc_id                  = aws_vpc.vpc_private.id
+  cidr_block              = "172.34.64.0/19"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    "Name"                       = "public-us-east-1a"
+    "kubernetes.io/role/elb"     = "1"
+    "kubernetes.io/cluster/demo" = "owned"
+  }
+}
+
+resource "aws_subnet" "public-us-east-1b" {
+  vpc_id                  = aws_vpc.vpc_private.id
+  cidr_block              = "172.34.96.0/19"
+  availability_zone       = "us-east-1b"
+  map_public_ip_on_launch = true
+
+  tags = {
+    "Name"                       = "public-us-east-1b"
+    "kubernetes.io/role/elb"     = "1"
+    "kubernetes.io/cluster/demo" = "owned"
+  }
+}
