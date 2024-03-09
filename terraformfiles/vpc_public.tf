9751bb6407cd6b0dbd152b72e1151f0ca83d7628
SAHICHOWDARY <78893457+Sahichowdary@users.noreply.github.com>
2024-03-09 11:40:20 +0530

    Update vpc_public.tf

--- terraformfiles/vpc_public.tf
+++ terraformfiles/vpc_public.tf
@@ -20,7 +20,23 @@ resource "aws_subnet" "vpc_public_subnet_public_1" {
 
 resource "aws_route_table" "public_vpc_route_table" {
   vpc_id = aws_vpc.vpc_public.id
-
+     route = [
+    {
+      cidr_block                 = "0.0.0.0/0"
+      gateway_id                 = aws_internet_gateway.igw.id
+      nat_gateway_id             = ""
+      carrier_gateway_id         = ""
+      destination_prefix_list_id = ""
+      egress_only_gateway_id     = ""
+      instance_id                = ""
+      ipv6_cidr_block            = ""
+      local_gateway_id           = ""
+      network_interface_id       = ""
+      transit_gateway_id         = ""
+      vpc_endpoint_id            = ""
+      vpc_peering_connection_id  = ""
+    },
+  ]
   tags = {
     Name = "public Route table"
   }
