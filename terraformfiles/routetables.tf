06616382e21b58f5d8038b9fe38745b685b8acbe
SAHICHOWDARY <78893457+Sahichowdary@users.noreply.github.com>
2024-03-09 12:04:50 +0530

    Update and rename routetables.tfx to routetables.tf

--- terraformfiles/routetables.tf
+++ terraformfiles/routetables.tf
@@ -51,21 +51,21 @@ resource "aws_route_table" "public" {
 }
 
 resource "aws_route_table_association" "private-us-east-1a" {
-  subnet_id      = aws_subnet.private-us-east-1a.id
+  subnet_id      = aws_subnet.vpc_private_subnet_private_1.id
   route_table_id = aws_route_table.private.id
 }
 
 resource "aws_route_table_association" "private-us-east-1b" {
-  subnet_id      = aws_subnet.private-us-east-1b.id
+  subnet_id      = aws_subnet.vpc_private_subnet_private_2.id
   route_table_id = aws_route_table.private.id
 }
 
 resource "aws_route_table_association" "public-us-east-1a" {
-  subnet_id      = aws_subnet.public-us-east-1a.id
+  subnet_id      = aws_subnet.vpc_public_subnet_public_1.id
   route_table_id = aws_route_table.public.id
 }
 
 resource "aws_route_table_association" "public-us-east-1b" {
-  subnet_id      = aws_subnet.public-us-east-1b.id
+  subnet_id      = aws_subnet.vpc_public_subnet_public_2.id
   route_table_id = aws_route_table.public.id
 }
