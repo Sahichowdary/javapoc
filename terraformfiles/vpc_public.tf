bfde9c136f466d5cc18b1602c7d7efef5d36a9c6
SAHICHOWDARY <78893457+Sahichowdary@users.noreply.github.com>
2024-03-09 12:06:06 +0530

    Update vpc_public.tf

--- terraformfiles/vpc_public.tf
+++ terraformfiles/vpc_public.tf
@@ -14,7 +14,16 @@ resource "aws_subnet" "vpc_public_subnet_public_1" {
   availability_zone = var.availability_zone
 
   tags = {
-    Name = "pocdemo-subnet-public1-us-west-2a"
+    Name = "pocdemo-subnet-public1-us-east-1a"
   }
 }
 
+resource "aws_subnet" "vpc_public_subnet_public_1" {
+  vpc_id     = aws_vpc.vpc_public.id
+  cidr_block = "172.33.16.0/20"
+  availability_zone = var.availability_zone2
+
+  tags = {
+    Name = "pocdemo-subnet-public2-us-east1b"
+  }
+}
